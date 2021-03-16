d98363efaee04813cc38f781fb9f3d7cbeacdd51
A PROJECT <77874962+ardhixsquerpants@users.noreply.github.com>
2021-03-16 16:13:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 pkg install nodejs -y
 pkg install ffmpeg -y
 pkg install imagemagick -y
-npm i
+npm install
 npm audit fix
 
-echo "[!] SUDAH SELESAI TINGGAL node index.js SOB:)"
-echo "JANGAN LUPA SUBREK CHANNEL A PROJECT NEW UPDATE:)"
+echo "SUDAH SELESAI TINGGAL node index.js SOB:)"
+echo "JANGAN LUPA SU[!]BREK CHANNEL A PROJECT NEW UPDATE:)"
+echo "[!] Bila terjadi error hapus folder node_modules lalu ketik npm i enter npm audit fix enter DONE:)"
