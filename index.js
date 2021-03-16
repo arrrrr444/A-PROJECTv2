c320dd086ef4ebaeadc995d1c42159058ce95ca9
A PROJECT <77874962+ardhixsquerpants@users.noreply.github.com>
2021-03-16 16:18:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('A PROJECT\nWhatsApp Bot', {
+CFonts.say('A PROJECTv2\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
